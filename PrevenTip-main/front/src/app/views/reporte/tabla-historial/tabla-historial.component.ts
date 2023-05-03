d57ce908e1b74a07c06bf0ea804d6f2c1ae1f029
SamMendez72 <A01652277@itesm.mx>
2023-05-03 17:50:21 -0600

    Update dato de tabla

--- PrevenTip-main/front/src/app/views/reporte/tabla-historial/tabla-historial.component.ts
+++ PrevenTip-main/front/src/app/views/reporte/tabla-historial/tabla-historial.component.ts
@@ -9,7 +9,7 @@ import { Medicion } from 'src/app/interfaces/medicion';
 export class TablaHistorialComponent implements OnInit{
   listProducts: Medicion[] = [
     {Ritmo: 1, Frec: 2, Peso: 3, Altura: 4, BMI: 6, Saturacion: 7, Temp:8, Presion:9},
-    {Ritmo: 11, Frec: 12, Peso: 13, Altura: 14, BMI: 16, Saturacion: 17, Temp:18, Presion:19},
+    {Ritmo: 23, Frec: 12, Peso: 13, Altura: 14, BMI: 16, Saturacion: 17, Temp:18, Presion:19},
   ]
   
 
